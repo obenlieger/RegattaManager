8926bc24bd52ede9a02d7a9df0b67681fad33eb5
Björn Ohlrich <bjoern.ohlrich@hotmail.de>
2018-04-28 16:26:09 +0200

    280420181626

--- RegattaManager/Migrations/20180428135436_speaker.Designer.cs
+++ RegattaManager/Migrations/20180428135436_speaker.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace RegattaManager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180428135436_speaker")]
+    partial class speaker
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
