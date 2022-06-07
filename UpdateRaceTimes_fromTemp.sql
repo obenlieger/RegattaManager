40e34ed6d5a75e43c43685fb2ee2b04af275953b
Björn Ohlrich <info@sc0tty.com>
2022-06-08 00:20:26 +0200

    202206080020

--- UpdateRaceTimes_fromTemp.sql
+++ UpdateRaceTimes_fromTemp.sql
@@ -5,4 +5,23 @@ UPDATE Races
 SET Races.Starttime = rtt.Starttime
 FROM Races r
 INNER JOIN RaceTimesTemp rtt ON r.RaceCode = rtt.RaceCode
-WHERE SUBSTRING(rtt.RaceCode,6,1) = 'E'
+
+UPDATE Races SET Comment = 'Abteilungslauf'
+  WHERE RaceCode like '11033V%' 
+     OR RaceCode like '11043V%'
+	 OR RaceCode like '12033V%'
+	 OR RaceCode like '12043V%'
+	 OR RaceCode like '11173V%'
+	 OR RaceCode like '11183V%'
+	 OR RaceCode like '12173V%'
+    AND RegattaId = 2
+
+UPDATE Races SET Comment = 'Pokallauf'
+  WHERE RaceCode = '11033E' 
+     OR RaceCode = '11043E'
+	 OR RaceCode = '12033E'
+	 OR RaceCode = '12043E'
+	 OR RaceCode = '11173E'
+	 OR RaceCode = '11183E'
+	 OR RaceCode = '12173E'
+	 AND RegattaId = 2
--- UpdateReportedRaces_Abteilungslauf.sql
+++ UpdateReportedRaces_Abteilungslauf.sql
@@ -1,3 +1,4 @@
 
   UPDATE ReportedRaces SET isAbteilungslauf = 1 
-  WHERE RaceCode IN ('11183E','11023E','11033E', '11043E', '12183E', '12023E', '12033E', '12043E') AND RegattaId = 2
+  WHERE RaceCode IN ('11183E','11023E','11033E', '11043E', '12183E', '12023E', '12033E', '12043E', '11083E', '12083E', '93003E') AND RegattaId = 2
+
