e72928ef56f903878504ee34d835bc33af7a7869
Björn Ohlrich <info@sc0tty.com>
2022-06-10 00:42:05 +0200

    202206100042

--- RegattaManager/Controllers/SpeakerController.cs
+++ RegattaManager/Controllers/SpeakerController.cs
@@ -34,14 +34,15 @@ namespace RegattaManager.Controllers
 
             if (rid != 0)
             {
-                var model = _context.Races.Include(e => e.Boatclass).Include(e => e.Oldclass).Include(e => e.Raceclass).Include(e => e.Regatta).Include(e => e.Racestatus).Include(e => e.Startboats).Include(e => e.RaceTyp).Where(e => e.RegattaId == rid && e.RacestatusId != 2 && e.RacestatusId != 1002 && e.RacestatusId != 1004 && e.RacestatusId != 1006 && e.ResultSpoken == false).OrderBy(e => e.Starttime);                
+                var model = _context.Races.Include(e => e.Boatclass).Include(e => e.Oldclass).Include(e => e.Raceclass).Include(e => e.Regatta).Include(e => e.Racestatus).Include(e => e.Startboats).Include(e => e.RaceTyp).Where(e => e.RegattaId == rid && e.RacestatusId != 2 && e.RacestatusId != 1002 && e.RacestatusId != 1004 && e.RacestatusId != 1006 && e.ResultSpoken == false).OrderBy(e => e.Starttime).ToList();                
                                 
                 ViewBag.startboats = _context.Startboats.Include(e => e.Club).OrderBy(e => e.Placement).ThenBy(e => e.Startslot).ToList();
                 ViewBag.startboatmembers = _context.StartboatMembers.ToList();
                 ViewBag.members = _context.Members.Include(e => e.Club).ToList(); 
-                ViewBag.disqsbs = _context.Startboats.Include(e => e.Club).Include(e => e.Startboatstatus).Where(e => e.Placement <= 0).OrderBy(e => e.Startslot).ToList();          
+                ViewBag.disqsbs = _context.Startboats.Include(e => e.Club).Include(e => e.Startboatstatus).Where(e => e.Placement <= 0).OrderBy(e => e.Startslot).ToList();
+                ViewBag.AllClubs = _context.Clubs.ToList();
 
-                return View(model.ToList());
+                return View(model);
             }
             else
             {
